0b35dc7ab91697014e04229875645b98e0444387
AvianLearning <iaradcliffe@hotmail.com>
2020-10-13 19:15:13 +0100

    Passed test to check can add paint can to stock

--- models/specs/decorator_specs.js
+++ models/specs/decorator_specs.js
@@ -1,10 +1,12 @@
 const assert = require('assert');
 const Decorator = require('../decorator.js');
+const Paint = require('../paint.js');
 
 describe('Decorator', function() {
     let decorator;
     beforeEach(function() {
         decorator = new Decorator(0);
+        paintCan = new Paint(2);
     })
     it('should have empty paint stock', function() {
         const actual = decorator.paintStock.length;
@@ -12,6 +14,7 @@ describe('Decorator', function() {
     });
 
     it('should be able to add can to paint stock', function() {
+        decorator.addCan(paintCan);
         const actual = decorator.paintStock.length;
         assert.strictEqual(actual, 1);
     })
